b20111d7e245d1da6b00e58d10b935e00448fd8b
pachanga <pacha.shevaev@gmail.com>
2019-03-21 17:49:42 +0300

    Starting to prepare testing infrastructure for VM implementation

---
+++ tests/test_vm.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+using Antlr4.Runtime;
+using bhl;
+
+public class BHL_TestVM : BHL_TestBase
+{
+}
