d5fd37636f516c2107e9b42579a8797d80ca2f52
dimus <dimus@localhost>
2019-04-08 12:16:59 +0300

    Improving bhl build for windows

--- bhl.bat
+++ bhl.bat
@@ -4,4 +4,13 @@ set DIR=%~dp0
 set SRC=%DIR%\bhl.cs
 set EXE=%DIR%\bhlb.exe
 
+FOR /F %%i IN ('DIR /B /O:D %SRC% %EXE%') DO SET NEWEST=%%i
+IF NOT x%NEWEST:.exe=% == x%NEWEST% GOTO RUN
+
 mcs %SRC% -debug -r:%DIR%\mono_opts.dll -out:%EXE% && mono --debug %EXE% %*
+GOTO END
+
+:RUN
+mono --debug %EXE% %*
+
+:END
