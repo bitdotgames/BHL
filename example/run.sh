ea610fcfcff33e6e2462d814c763cb5215ef3794
pachanga <pacha.shevaev@gmail.com>
2019-04-08 10:44:04 +0300

    Getting rid of PHP as a build tool

--- example/run.sh
+++ example/run.sh
@@ -8,7 +8,7 @@ fi
 
 #1. Running frontend over bhl sources
 rm tmp/bhl.bytes
-php ../bhl -D USER_SOURCES=bindings.cs run -C --dir=. --result=tmp/bhl.bytes --cache_dir=tmp --error=tmp/bhl.err
+../bhl run --user-sources=bindings.cs -C --dir=. --result=tmp/bhl.bytes --cache_dir=tmp --error=tmp/bhl.err
 
 if [ $? -ne 0 ] ;
 then
@@ -27,14 +27,14 @@ set -e
 if [ "$1" == "-unity" ] ;
 then
   #2. Building bhl backend dll
-  php ../bhl build_back_dll $UNITY_MONO_PATH/bin/gmcs  
+  ../bhl build_back_dll $UNITY_MONO_PATH/bin/gmcs  
   #3. Building example: adding bhl backend dll, user bindings 
   $UNITY_MONO_PATH/bin/gmcs -r:../bhl_back.dll -out:example.exe bindings.cs example.cs
   #4. Running example
   MONO_PATH=$UNITY_MONO_PATH/lib/mono/2.0/:../ $UNITY_MONO_PATH/bin/mono --debug example.exe
 else
   #2. Building bhl backend dll
-  php ../bhl build_back_dll mcs 
+  ../bhl build_back_dll mcs 
   #3. Building example: adding bhl backend dll, user bindings 
   mcs -r:../bhl_back.dll -out:example.exe bindings.cs example.cs
   MONO_PATH=$MONO_PATH:../ mono --debug example.exe
