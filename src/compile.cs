15c8b201cfd3ef59bf89e2d5e1133b69ec1d993b
pachanga <pacha.shevaev@gmail.com>
2019-05-04 07:41:59 +0300

    Minor stylistic changes

--- src/compile.cs
+++ src/compile.cs
@@ -90,8 +90,8 @@ public class Compiler : AST_Visitor
     public UInt16[] operand_width;
   }
 
-  Opcode Op_Constant = new Opcode() { opcode = 1 };
-  Opcode Op_Add = new Opcode() { opcode = 2 };
+  static readonly Opcode Op_Constant = new Opcode() { opcode = 1 };
+  static readonly Opcode Op_Add = new Opcode() { opcode = 2 };
 
   private Dictionary<Opcode, Definition> definitions = new Dictionary<Opcode, Definition>();
 
@@ -100,22 +100,23 @@ public class Compiler : AST_Visitor
     definitions.Add(Op_Constant,
                     new Definition()
                     {
-                            name = "Op_Constant",
-                            operand_width = new UInt16[] { 2 }
+                      name = "Op_Constant",
+                      operand_width = new UInt16[] { 2 }
                     }
     );
     definitions.Add(Op_Add,
                     new Definition()
                     {
-                            name = "Op_Add",
-                            operand_width = new UInt16[] { 0 }
+                      name = "Op_Add",
+                      operand_width = new UInt16[] { 0 }
                     }
     );
   }
-  private Definition Lookup(Opcode op)
+
+  Definition Lookup(Opcode op)
   {
     var def = definitions[op];
-    if (def.Equals(null))
+    if(def.Equals(null))
        return new Definition() { name = "Exception", operand_width = null };//looks like shit;
     return def;
   }
@@ -123,22 +124,20 @@ public class Compiler : AST_Visitor
   public byte[] Make(Opcode op, UInt16[] operands)//?
   {
     var def = Lookup(op);
-    if (def.operand_width == null)
-    { 
+    if(def.operand_width == null)
       return new byte[0];
-    }
+
     UInt16 instruction_length = 1;
-    foreach (var d in def.operand_width)
-    {
+    foreach(var d in def.operand_width)
       instruction_length += d;
-    }
+
     var instruction = new byte[instruction_length];//dynamic array
     instruction[0] = op.opcode;
     UInt16 offset = 1;
-    for (int i = 0; i < operands.Length; i++)
+    for(int i = 0; i < operands.Length; i++)
     {
       var width = def.operand_width[i];
-      switch (width)
+      switch(width)
       {
         case 2:
          PutUint(instruction, operands[i], offset);// where, what, offset//littlEndian
@@ -146,7 +145,7 @@ public class Compiler : AST_Visitor
       }
       offset += width;
     }
-      return instruction;
+    return instruction;
   }
 
   private void PutUint(byte[] insert, UInt16 ui, UInt16 offset)
@@ -181,7 +180,7 @@ public class Compiler : AST_Visitor
 
   public override void DoVisit(AST_FuncDecl ast)
   {
-    Console.WriteLine("FUNC " + ast.name + " :");
+    //Console.WriteLine("FUNC " + ast.name + " :");
     VisitChildren(ast);
   }
 
@@ -241,9 +240,9 @@ public class Compiler : AST_Visitor
   public override void DoVisit(AST_BinaryOpExp node)
   {
     byte[] res = null;
-    switch (node.type.ToString())
+    switch(node.type)
     {
-      case "ADD":
+      case EnumBinaryOp.ADD:
         res = Make(Op_Add, new UInt16[0]);
       break;
     }
