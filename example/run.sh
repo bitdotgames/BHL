527346415e7eb1b2a1401daf1bb2293516bf5d2d
Pavel Shevaev <pacha.shevaev@gmail.com>
2022-03-03 10:22:41 +0300

    Restoring example

--- example/run.sh
+++ example/run.sh
@@ -14,14 +14,17 @@ function mytrap {
 
 trap mytrap ERR
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 #1. Building bhl backend dll
-../bhl build_back_dll mcs 
+(cd .. && ./bhl build_back_dll mcs) 
 #2. Building example: adding bhl backend dll, user bindings 
-mcs -r:../bhl_back.dll -out:example.exe bindings.cs example.cs
+mcs -r:../bhl_back.dll -out:example.exe $DIR/bindings.cs $DIR/example.cs
 
 #3. Compiling bhl sources to byte code
 rm -rf tmp/bhl.bytes
 rm -rf tmp/bhl.err
-../bhl compile --user-sources=bindings.cs -C --dir=. --result=tmp/bhl.bytes --tmp-dir=tmp --error=tmp/bhl.err
+(cd .. && ./bhl compile --user-sources=$DIR/bindings.cs -C --dir=$DIR --result=$DIR/tmp/bhl.bytes --tmp-dir=$DIR/tmp --error=$DIR/tmp/bhl.err)
 #4. Running example
 MONO_PATH=$MONO_PATH:../ mono --debug example.exe tmp/bhl.bytes
+
