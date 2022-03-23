f9a490aaa89933657d90865384e91e9b650fb8c2
Pavel Shevaev <pacha.shevaev@gmail.com>
2022-03-23 10:59:41 +0300

    Adding new interface tests

--- tests/test_interface.cs
+++ tests/test_interface.cs
@@ -36,4 +36,38 @@ public class TestInterfaces : BHL_TestBase
     AssertEqual(Types.Float, hey.arg_types[1].Get());
     AssertEqual(Types.Void, hey.ret_type.Get());
   }
+
+  [IsTested()]
+  public void TestUserInterfaceWithSeveralMethods()
+  {
+    string bhl = @"
+    class Bar { 
+    }
+
+    interface Foo { 
+      func bool hey(int a, float b)
+      func Bar,int bar(string s)
+    }
+    ";
+
+    var vm = MakeVM(bhl);
+    var symb = vm.Types.Resolve("Foo") as InterfaceSymbolScript;
+    AssertTrue(symb != null);
+
+    var hey = symb.FindSignature("hey");
+    AssertTrue(hey != null);
+    AssertEqual(2, hey.arg_types.Count);
+    AssertEqual(Types.Int, hey.arg_types[0].Get());
+    AssertEqual(Types.Float, hey.arg_types[1].Get());
+    AssertEqual(Types.Bool, hey.ret_type.Get());
+
+    var bar = symb.FindSignature("bar");
+    AssertTrue(bar != null);
+    AssertEqual(1, bar.arg_types.Count);
+    AssertEqual(Types.String, bar.arg_types[0].Get());
+    var tuple = (TupleType)bar.ret_type.Get();
+    AssertEqual(2, tuple.Count);
+    AssertEqual("Bar", tuple[0].name);
+    AssertEqual(Types.Int, tuple[1].Get());
+  }
 }
