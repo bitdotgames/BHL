15fd2bf66b0a23e1f6fcf5f8392a148cd7def91c
Pavel Shevaev <pacha.shevaev@gmail.com>
2022-05-06 11:19:06 +0300

    Refactoring namespaces WIP

--- example/bindings.cs
+++ example/bindings.cs
@@ -25,7 +25,7 @@ public class MyBindings : IUserBindings
         new FuncArgSymbol("str", Types.String)
         );
 
-      types.ns.Define(fn);
+      types.default_ns.Define(fn);
     }
 
     {
@@ -40,7 +40,7 @@ public class MyBindings : IUserBindings
           return null;
         }
       );
-      types.ns.Define(fn);
+      types.default_ns.Define(fn);
     }
 
     {
@@ -50,7 +50,7 @@ public class MyBindings : IUserBindings
           new FuncArgSymbol("t", Types.Float)
         );
 
-      types.ns.Define(fn);
+      types.default_ns.Define(fn);
     }
   }
 }
