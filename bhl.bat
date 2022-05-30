b7956229d3bfe93d812f29df9b2c7c36e69c18dd
Pavel Shevaev <pacha.shevaev@gmail.com>
2022-05-30 13:40:03 +0300

    Experimenting with Windows MONO_PATH

--- bhl.bat
+++ bhl.bat
@@ -3,7 +3,6 @@
 set DIR=%~dp0
 set SRC=%DIR%\bhl.cs
 set EXE=%DIR%\bhlb.exe
-set PATH=deps;%PATH%
 set MONO_PATH=%DIR%\deps;%MONO_PATH%
 
 FOR /F %%i IN ('DIR /B /O:D %SRC% %EXE%') DO SET NEWEST=%%i
