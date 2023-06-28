584c3fc7c7576237b78b2634e0811ff908559117
Pavel Shevaev <pacha.shevaev@gmail.com>
2023-06-29 00:08:04 +0300

    Moving built .dll and .exe products into build/ directory

--- bhl.bat
+++ bhl.bat
@@ -3,6 +3,7 @@
 set DIR=%~dp0
 set SRC=%DIR%\bhl.cs
 set EXE=%DIR%\build\bhlb.exe
+mkdir "%DIR%\build"
 set MONO_PATH=%DIR%\build;%DIR%\deps;%MONO_PATH%
 
 FOR /F %%i IN ('DIR /B /O:D %SRC% %EXE%') DO SET NEWEST=%%i
