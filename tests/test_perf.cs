c89662ea074ca0b782a3709883fc31039efcd417
Pavel Shevaev <pacha.shevaev@gmail.com>
2024-05-21 16:17:00 +0300

    Adding more performance related tests

--- tests/test_perf.cs
+++ tests/test_perf.cs
@@ -118,7 +118,7 @@ public class TestPerf : BHL_TestBase
       AssertFalse(vm.Tick());
       stopwatch.Stop();
       AssertEqual(fb.result.PopRelease().num, 610);
-      Console.WriteLine("fib import ticks: {0}", stopwatch.ElapsedTicks);
+      Console.WriteLine("fib imported ticks: {0}", stopwatch.ElapsedTicks);
     }
 
     {
@@ -128,7 +128,93 @@ public class TestPerf : BHL_TestBase
       AssertFalse(vm.Tick());
       stopwatch.Stop();
       AssertEqual(fb.result.PopRelease().num, 610);
-      Console.WriteLine("fib import ticks2: {0}", stopwatch.ElapsedTicks);
+      Console.WriteLine("fib imported ticks2: {0}", stopwatch.ElapsedTicks);
+    }
+
+    CommonChecks(vm);
+  }
+  
+  [IsTested()]
+  public void TestFibonacciMethodImported()
+  {
+    string fib1 = @"
+    import ""fib2""
+
+    class Fib1
+    {
+      func int fib1(int x, Fib2 f2)
+      {
+        if(x == 0) {
+          return 0
+        } else {
+          if(x == 1) {
+            return 1
+          } else {
+            return f2.fib2(x - 1, this) + f2.fib2(x - 2, this)
+          }
+        }
+      }
+    }
+    ";
+
+    string fib2 = @"
+    import ""fib1""
+
+    class Fib2
+    {
+      func int fib2(int x, Fib1 f1)
+      {
+        if(x == 0) {
+          return 0
+        } else {
+          if(x == 1) {
+            return 1
+          } else {
+            return f1.fib1(x - 1, this) + f1.fib1(x - 2, this)
+          }
+        }
+      }
+     }
+    ";
+
+    string test = @"
+    import ""fib1""
+    import ""fib2""
+
+    func int test() 
+    {
+      var f1 = new Fib1
+      var f2 = new Fib2
+      int x = 15 
+      return f1.fib1(x, f2)
+    }
+    ";
+
+    var vm = MakeVM(new Dictionary<string, string>() {
+        {"fib1.bhl", fib1},
+        {"fib2.bhl", fib2},
+        {"test.bhl", test},
+      }
+    );
+
+    {
+      var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+      vm.LoadModule("test");
+      var fb = vm.Start("test");
+      AssertFalse(vm.Tick());
+      stopwatch.Stop();
+      AssertEqual(fb.result.PopRelease().num, 610);
+      Console.WriteLine("fib method imported ticks: {0}", stopwatch.ElapsedTicks);
+    }
+
+    {
+      var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+      vm.LoadModule("test");
+      var fb = vm.Start("test");
+      AssertFalse(vm.Tick());
+      stopwatch.Stop();
+      AssertEqual(fb.result.PopRelease().num, 610);
+      Console.WriteLine("fib method imported ticks2: {0}", stopwatch.ElapsedTicks);
     }
 
     CommonChecks(vm);
