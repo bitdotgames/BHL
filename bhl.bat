956cc2f6fdfe9ba9b5cdbc2799c1b211f2ca6c99
Pavel Shevaev <pacha.shevaev@gmail.com>
2024-09-20 15:32:57 +0300

    Tweaking bhl.bat for Windows

--- bhl.bat
+++ bhl.bat
@@ -2,7 +2,19 @@
 
 set DIR=%~dp0
 set BHL_DLL=%DIR%\build\Debug\bhl\net7.0\bhl.dll
+set VERSION=%$DIR%\src\vm\version.cs
 
-dotnet %BHL_DLL% %*
+FOR %%f in (%VERSION%) do set VERSION_T=%%~tf
+FOR %%f in (%BHL_DLL%) do set BHL_DLL_T=%%~tf
+IF %VERSION_T:~0, 10% LSS %BHL_DLL_T:~0, 10% GOTO :RUN
+IF EXIST %BHL_DLL% GOTO :RUN
 
+dotnet publish %DIR%\bhl.csproj || GOTO :ERROR
+
+:RUN
+dotnet %BHL_DLL% %* || GOTO :ERROR
+GOTO :EOF
+
+:ERROR
+echo Failed with error #%errorlevel%
 EXIT /b %errorlevel%
