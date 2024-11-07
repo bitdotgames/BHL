d936eadfea7e597561ca9f07699be7af72c56bae
Pavel Shevaev <pacha.shevaev@gmail.com>
2024-11-07 16:03:29 +0300

    Experimenting with LSP completion WIP

--- src/lsp/services/completion.cs
+++ src/lsp/services/completion.cs
@@ -29,16 +29,30 @@ public class TextDocumentCompletionService : IService
   [RpcMethod("textDocument/completion")]
   public Task<RpcResult> Completion(CompletionParams args)
   {
+    var document = workspace.GetOrLoadDocument(args.textDocument.uri);
+    
     var list = new CompletionList();
     list.isIncomplete = false;
-    
-    list.items = new List<CompletionItem>();
 
-    //TODO:
-    //var item = new CompletionItem();
-    //item.label = "Boo";
-    //item.insertText = item.label;
-    //list.items.Add(item);
+    if(document != null)
+    {
+      //var symb = document.FindSymbol((int)args.position.line, (int)args.position.character);
+      
+      var node = document.FindTerminalNode((int)args.position.line, (int)args.position.character-1);
+      if(node != null)
+      {
+        Logger.current.Log(0, "NODE " + node.GetType().Name + " " + node.GetText() + " " + node.GetHashCode() + "; parent " + node.Parent.GetType().Name + " " + node.Parent.GetText());
+        
+      }
+      
+      list.items = new List<CompletionItem>();
+
+      //TODO:
+      //var item = new CompletionItem();
+      //item.label = "Boo";
+      //item.insertText = item.label;
+      //list.items.Add(item);
+    }
     
     return Task.FromResult(new RpcResult(list));
   }
