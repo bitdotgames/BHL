cde0398bdd707a8e22f123f1dda635ebe076abf5
Pavel Shevaev <pacha.shevaev@gmail.com>
2024-11-07 19:16:03 +0300

    Experimenting with LSP completion WIP

--- src/lsp/services/completion.cs
+++ src/lsp/services/completion.cs
@@ -34,24 +34,30 @@ public class TextDocumentCompletionService : IService
     var list = new CompletionList();
     list.isIncomplete = false;
 
+    var completions = new HashSet<Symbol>();
     if(document != null)
     {
-      //var symb = document.FindSymbol((int)args.position.line, (int)args.position.character);
-      
       var node = document.FindTerminalNode((int)args.position.line, (int)args.position.character-1);
       if(node != null)
       {
-        Logger.current.Log(0, "NODE " + node.GetType().Name + " " + node.GetText() + " " + node.GetHashCode() + "; parent " + node.Parent.GetType().Name + " " + node.Parent.GetText());
-        
+        //Logger.current.Log(0, "NODE " + node.GetType().Name + " " + node.GetText() + " " + node.GetHashCode() + "; parent " + node.Parent.GetType().Name + " " + node.Parent.GetText());
+
+        foreach(var kv in document.proc.annotated_nodes)
+        {
+          if(kv.Value.lsp_symbol != null && kv.Value.lsp_symbol.name.Contains(node.GetText())) 
+            completions.Add(kv.Value.lsp_symbol);
+        }
       }
       
       list.items = new List<CompletionItem>();
 
-      //TODO:
-      //var item = new CompletionItem();
-      //item.label = "Boo";
-      //item.insertText = item.label;
-      //list.items.Add(item);
+      foreach (var symbol in completions)
+      {
+        var item = new CompletionItem();
+        item.label = symbol.name;
+        item.insertText = item.label;
+        list.items.Add(item);
+      }
     }
     
     return Task.FromResult(new RpcResult(list));
