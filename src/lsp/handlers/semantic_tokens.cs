146d05009c05fe391f12bda7d5b2210a1dd10629
pachanga <pacha.shevaev@gmail.com>
2025-10-02 19:52:02 +0300

    Restoring semantic tokens test

--- src/lsp/handlers/semantic_tokens.cs
+++ src/lsp/handlers/semantic_tokens.cs
@@ -20,43 +20,14 @@ public class SemanticTokensHandler : SemanticTokensHandlerBase
     _workspace = workspace;
   }
 
-  public override async Task<SemanticTokens?> Handle(SemanticTokensParams request, CancellationToken ct)
-  {
-    return await base.Handle(request, ct).ConfigureAwait(false);
-  }
-
-  public override async Task<SemanticTokens?> Handle(SemanticTokensRangeParams request, CancellationToken ct)
-  {
-    return await base.Handle(request, ct).ConfigureAwait(false);
-  }
-
-  public override async Task<SemanticTokensFullOrDelta?> Handle(SemanticTokensDeltaParams request, CancellationToken ct)
-  {
-    return await base.Handle(request, ct).ConfigureAwait(false);
-  }
-
-  protected override Task Tokenize(SemanticTokensBuilder builder, ITextDocumentIdentifierParams identifier, CancellationToken ct)
-  {
-    var document = _workspace.GetOrLoadDocument(identifier.TextDocument.Uri);
-
-    foreach (var token in document.Processed.semantic_tokens)
-    {
-      int line = token.line - 1;
-      int column = token.column;
-      builder.Push(line, column, token.len, (int)token.type_idx, (int)token.mods);
-    }
-    return Task.CompletedTask;
-  }
-
-  protected override Task<SemanticTokensDocument>
-    GetSemanticTokensDocument(ITextDocumentIdentifierParams @params, CancellationToken cancellationToken)
-  {
-    return Task.FromResult(new SemanticTokensDocument(RegistrationOptions.Legend));
-  }
-
   protected override SemanticTokensRegistrationOptions CreateRegistrationOptions(
     SemanticTokensCapability capability, ClientCapabilities clientCapabilities
   )
+  {
+    return MakeRegistrationOptions();
+  }
+
+  static SemanticTokensRegistrationOptions MakeRegistrationOptions()
   {
     //NOTE: synchronized with ANTLR_Processor.token_types
     IEnumerable<SemanticTokenType> tokenTypes = new []
@@ -81,7 +52,7 @@ public class SemanticTokensHandler : SemanticTokensHandlerBase
       SemanticTokenModifier.Static
     };
 
-    return new SemanticTokensRegistrationOptions
+    return new ()
     {
       DocumentSelector = TextDocumentSelector.ForLanguage("bhl"),
       Legend = new SemanticTokensLegend
@@ -95,5 +66,41 @@ public class SemanticTokensHandler : SemanticTokensHandlerBase
       },
       Range = true
     };
+  }
+
+  public override async Task<SemanticTokens?> Handle(SemanticTokensParams request, CancellationToken ct)
+  {
+    return await base.Handle(request, ct);
+  }
+
+  public override async Task<SemanticTokens?> Handle(SemanticTokensRangeParams request, CancellationToken ct)
+  {
+    return await base.Handle(request, ct);
+  }
+
+  public override async Task<SemanticTokensFullOrDelta?> Handle(SemanticTokensDeltaParams request, CancellationToken ct)
+  {
+    return await base.Handle(request, ct);
+  }
+
+  protected override async Task Tokenize(SemanticTokensBuilder builder, ITextDocumentIdentifierParams identifier, CancellationToken ct)
+  {
+    await _workspace.SetupProjectIfEmptyAsync(identifier.TextDocument.Uri.Path);
+
+    var document = _workspace.GetOrLoadDocument(identifier.TextDocument.Uri);
+
+    foreach (var token in document.Processed.semantic_tokens)
+    {
+      int line = token.line - 1;
+      int column = token.column;
+      builder.Push(line, column, token.len, (int)token.type_idx, (int)token.mods);
+    }
+  }
+
+  protected override Task<SemanticTokensDocument> GetSemanticTokensDocument(ITextDocumentIdentifierParams @params, CancellationToken cancellationToken)
+  {
+    //TODO: investigate why RegistrationOptions are null
+    //return Task.FromResult(new SemanticTokensDocument(RegistrationOptions.Legend));
+    return Task.FromResult(new SemanticTokensDocument(MakeRegistrationOptions().Legend));
   }
 }
--- tests/lsp/test_lsp_semantic_tokens.cs
+++ tests/lsp/test_lsp_semantic_tokens.cs
@@ -1,40 +1,53 @@
+using System;
+using System.Collections.Immutable;
 using System.Threading.Tasks;
 using bhl;
 using bhl.lsp;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using Xunit;
 
-//public class TestLSPSemanticTokens : TestLSPShared
-//{
-//  [Fact]
-//  public async Task TestSemanticTokensBasic()
-//  {
-//    string bhl = @"
-//    class BHL_M3Globals
-//    {
-//      []string match_chips_sfx
-//    }
-//    ";
-//
-//    var ws = new Workspace();
-//
-//    var srv = new ServerCreator(NoLogger(), NoConnection(), ws);
-//    srv.AttachService(new bhl.lsp.TextDocumentSemanticTokensService(srv));
-//
-//    CleanTestFiles();
-//
-//    var uri1 = MakeTestDocument("bhl1.bhl", bhl);
-//
-//    var ts = new bhl.Types();
-//    ws.Init(ts, GetTestProjConf());
-//    ws.IndexFiles();
-//
-//    var json = "{\"id\": 1,\"jsonrpc\": \"2.0\", \"method\": \"textDocument/semanticTokens/full\", \"params\":";
-//    json += "{\"textDocument\": {\"uri\": \"" + uri1.ToString() + "\"}}}";
-//
-//    AssertEqual(
-//      await srv.Handle(json),
-//      "{\"id\":1,\"result\":{\"data\":" +
-//      "[1,4,5,6,0," + "0,6,13,0,0," + "2,8,6,5,0," + "0,7,15,2,2]},\"jsonrpc\":\"2.0\"}"
-//    );
-//  }
-//}
+public class TestLSPSemanticTokens : TestLSPShared, IDisposable
+{
+  TestLSPHost srv;
+
+  public TestLSPSemanticTokens()
+  {
+    srv = NewTestServer();
+
+    CleanTestFiles();
+  }
+
+  public void Dispose()
+  {
+    srv.Dispose();
+  }
+
+  [Fact]
+  public async Task TestSemanticTokensBasic()
+  {
+    string bhl = @"
+    class BHL_M3Globals
+    {
+      []string match_chips_sfx
+    }
+    ";
+
+    var uri1 = MakeTestDocument("bhl1.bhl", bhl);
+
+    await SendInit(srv);
+
+    var result =
+      await srv.SendRequestAsync<SemanticTokensParams, SemanticTokens>(
+        "textDocument/semanticTokens/full",
+        new ()
+        {
+          TextDocument = uri1
+        }
+      );
+
+    //NOTE: used to be before migration to Omnisharp framework, check the difference?
+    //{1,4,5,6,0, + 0,6,13,0,0, + 2,8,6,5,0, + 0,7,15,2,2}
+    Assert.Equal(new int[] {1,4,5,6,0, 0,6,13,0,0, 2,15,15,2,2, 0,-7,6,5,0}, result.Data);
+  }
+}
