a5b2d7a54204ede2915ed06fd0df8c662b998b03
pachanga <pacha.shevaev@gmail.com>
2025-10-21 15:02:38 +0300

    Moving stuff around

--- tests/test_perf.cs
+++ tests/test_perf.cs
@@ -22,9 +22,9 @@ public class TestPerf : BHL_TestBase
       }
     }
 
-    func int test() 
+    func int test()
     {
-      int x = 15 
+      int x = 15
       return fib(x)
     }
     ";
@@ -52,5 +52,41 @@ public class TestPerf : BHL_TestBase
     }
 
     CommonChecks(vm);
+  }
+
+  [Fact]
+  public void TestFibonacci2()
+  {
+    string bhl = @"
+    func int fib(int x)
+    {
+      if(x == 0) {
+        return 0
+      } else {
+        if(x == 1) {
+          return 1
+        } else {
+          return fib(x - 1) + fib(x - 2)
+        }
+      }
+    }
+    ";
+
+    var vm = MakeVM(bhl, show_bytes: true);
+    {
+      var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+      var fb = vm.Start2("fib", Val2.NewInt(vm, 15));
+      Assert.False(vm.Tick());
+      Console.WriteLine("fib ticks: {0}", stopwatch.ElapsedTicks);
+      Assert.Equal(610, fb.exec.stack2.PopRelease().num);
+    }
+
+    {
+      var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+      var fb = vm.Start2("fib", Val2.NewInt(vm, 15));
+      Assert.False(vm.Tick());
+      Console.WriteLine("fib ticks2: {0}", stopwatch.ElapsedTicks);
+      Assert.Equal(610, fb.exec.stack2.PopRelease().num);
+    }
   }
 }
