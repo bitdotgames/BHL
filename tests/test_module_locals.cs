aa4df4274b1a651bcb09980f715dc8ca646f38c5
pachanga <pacha.shevaev@gmail.com>
2025-11-20 12:45:52 +0300

    Better file names

--- tests/test_module_locals.cs
+++ tests/test_module_locals.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using bhl;
 using Xunit;
 
-public class TestLocal : BHL_TestBase
+public class TestModuleLocals : BHL_TestBase
 {
   [Fact]
   public void TestLocalModuleFunc()
