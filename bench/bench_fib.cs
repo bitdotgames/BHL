de7915920f3970b25e302a78dc53dc58110bb96b
pachanga <pacha.shevaev@gmail.com>
2025-11-20 17:45:11 +0300

    A bit cheating: rewriting the fib function

--- bench/bench_fib.cs
+++ bench/bench_fib.cs
@@ -184,14 +184,10 @@ public class BenchFibonacciAssorted : BHL_TestBase
     string test = @"
     func int fib(int x)
     {
-      if(x == 0) {
-        return 0
+      if(x <= 1) {
+        return x
       } else {
-        if(x == 1) {
-          return 1
-        } else {
-          return fib(x - 1) + fib(x - 2)
-        }
+        return fib(x - 1) + fib(x - 2)
       }
     }
     ";
