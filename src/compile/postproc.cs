359adafa5cbe08640ba8d48ea718aa8ac0949629
a.chubar <a.chubar@bit.games>
2025-12-09 13:28:37 +0300

    Basic impl for combined AST postproc

--- src/compile/postproc.cs
+++ src/compile/postproc.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace bhl;
 
 public interface IFrontPostProcessor
@@ -16,5 +18,30 @@ public class EmptyPostProcessor : IFrontPostProcessor
 
   public void Tally()
   {
+  }
+}
+
+public class CombinedPostProcessor : IFrontPostProcessor
+{
+  //List instead of Enumerable to preserve the specified order
+  readonly IList<IFrontPostProcessor> _postprocessors;
+
+  public CombinedPostProcessor(IList<IFrontPostProcessor> postprocessors)
+  {
+    _postprocessors = postprocessors;
+  }
+
+  public ANTLR_Processor.Result Patch(ANTLR_Processor.Result fres, string src_file)
+  {
+    for(int i = 0; i < _postprocessors.Count; i++)
+      fres = _postprocessors[i].Patch(fres, src_file);
+
+    return fres;
+  }
+
+  public void Tally()
+  {
+    for(int i = 0; i < _postprocessors.Count; i++)
+      _postprocessors[i].Tally();
   }
 }
