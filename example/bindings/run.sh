d31eaff524f0371bcb9ed57a2f75179b53f748af
pachanga <pacha.shevaev@gmail.com>
2026-01-27 15:06:35 +0300

    Getting rid of TargetFramework specification in every .csproj file, using Directory.Build.props instead. For now getting rid of packages.lock.json files

--- example/bindings/run.sh
+++ example/bindings/run.sh
@@ -4,7 +4,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-rm -rf $DIR/tmp/bhl.bytes 
+rm -rf $DIR/tmp/bhl.bytes
 rm -rf $DIR/bin
 rm -rf $DIR/obj
 
@@ -12,4 +12,4 @@ rm -rf $DIR/obj
 $DIR/../../bhl compile -p $DIR/bhl.proj --result=$DIR/tmp/bhl.bytes
 
 #Running example
-dotnet run --framework net8.0 --project $DIR/example.csproj -- $DIR/tmp/bhl.bytes
+dotnet run --project $DIR/example.csproj -- $DIR/tmp/bhl.bytes
